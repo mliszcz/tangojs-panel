f09d5371bf8226b519aaf41ee143ce2e0582e89a
mliszcz <liszcz.michal@gmail.com>
2017-03-01 15:13:48 +0100

    wip changes for sync

---
+++ test/reducers.test.js
@@ -0,0 +1,11 @@
+
+import widgets from './reducers'
+
+describe('reducers.widgets', () => {
+
+  describe('ADD_WIDGET', () => {
+    it('should add widget to the collection', () => {
+      //
+    })
+  })
+})
